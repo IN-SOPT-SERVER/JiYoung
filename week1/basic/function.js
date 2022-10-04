c5d4e48fd67768b6ff8aaa610504e4c8aa997d1a
yangjiyoung <0112jyoung@naver.com>
2022-10-04 20:07:33 +0900

    1차 세미나 복습

---
+++ week1/basic/function.js
@@ -0,0 +1,13 @@
+function hello(name){
+    console.log(`안뇽 ${name}`);
+}
+
+hello("jiyong");
+
+const sum = (a,b) => {
+    console.log(`더한 값은 ${a+b}`);
+};
+
+sum("1","2");
+
+console.log(typeof sum);
---
+++ week1/basic/test.ts
@@ -0,0 +1,45 @@
+interface Members{
+    name: string;
+    age : number;
+    home: string;
+    favorite: string; 
+    mbti: string;
+    sibling: Boolean;
+    appjame?:Boolean;
+    study?:Boolean;
+}
+
+const members: Members[]=[
+    {
+        name:"이종현",
+        age:26,
+        home:"노원",
+        favorite:"헬스",
+        mbti: "enfj",
+        sibling: true
+    },{
+        name:"한유진",
+        age:24,
+        home:"잠실",
+        favorite:"운동",
+        mbti: "isfj",
+        sibling:true
+    },{
+        name:"장한빛",
+        age: 24,
+        home: "상봉",
+        favorite: "웹툰",
+        mbti:"esfp",
+        sibling:true
+    },{
+        name:"양지영",
+        age:25,
+        home: "청량리",
+        favorite: "아이스아메리카노",
+        mbti: "infj",
+        sibling:true
+    }
+]
+
+members.map((item) => console.log(`이름은 ${item.name}, 나이는 ${item.age}, 집은 ${item.home}, 좋아하는 것은 ${item.favorite},
+mbti는 ${item.mbti}, 형제자매 유무는 ${item.sibling} 입니다`));
