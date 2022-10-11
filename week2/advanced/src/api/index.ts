4a9ffe7b51a99cd37ddf621ac89bfeac4a5f09ce
yangjiyoung <0112jyoung@naver.com>
2022-10-11 19:19:32 +0900

    2차 세미나 과제 - 라우팅 실습

---
+++ week2/advanced/src/api/index.ts
@@ -0,0 +1,14 @@
+import express, { Router, Request, Response } from "express";
+
+const router: Router = express.Router(); // express 라우팅 시스템을 받아올거!
+
+router.get("/", (req: Request, res: Response) => {
+    return res.status(200).json({
+    status: 200,
+    message: "index.ts 들어오기 ",
+    });
+});
+
+router.use("/user", require("./user"));
+
+module.exports = router; // 모듈로 반환
--- week2/advanced/src/index.ts
+++ week2/advanced/src/index.ts
@@ -3,6 +3,8 @@ import express, {NextFunction, Request, Response} from "express";
 const app=express();
 const PORT=3000;
 
+app.use("/api", require("./api"));
+
 app.get("/", (req:Request, res:Response, next:NextFunction)=>{
     res.send("서버입니다람쥐");
 });
