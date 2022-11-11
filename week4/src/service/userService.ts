a93965fd2cc1f3376d1c300cb91abc69e81d5a79
yangjiyoung <0112jyoung@naver.com>
2022-11-11 20:14:01 +0900

    4차 세미나 - 서비스 설정

--- week4/src/service/userService.ts
+++ week4/src/service/userService.ts
@@ -12,8 +12,47 @@ const getUserById = async (userId: number) => {
   return user;
 };
 
-const userService = {
-  getUserById,
+//* 유저 생성
+const createUser = async(userName:string, email:string, age:number) => {
+  const data= await prisma.user.create({
+      data: {
+        userName: userName,
+        email: email,
+        age: age
+      }
+  });
+  return data;
 };
 
-export default userService;
+// * 유저 전체 조회
+const getAllUser = async() => {
+    const data= await prisma.user.findMany();
+
+    return data;
+};
+
+// * 유저 정보 업데이트
+const updateUser= async(userId:number, name: string) => {
+    const data= await prisma.user.update({
+      where:{
+        id:userId
+      },
+      data:{
+        userName:name
+      }
+    });
+    return data;
+};
+
+// * 유저 삭제 
+const deleteUser = async(userId:number) => {
+  await prisma.user.delete({
+    where: {
+      id: userId
+    }
+  });
+};
+
+const userService = {
+  getUserById,createUser, getAllUser, updateUser, deleteUser
+};
