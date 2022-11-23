ad5e4dd1049033b060807aee694417569ceae93e
yangjiyoung <0112jyoung@naver.com>
2022-11-23 20:47:51 +0900

    6차 세미나 - 회원 가입, 로그인 설정

--- week4/src/service/userService.ts
+++ week4/src/service/userService.ts
@@ -1,3 +1,7 @@
+import { UserCreateDTO } from "../interface/UserCreateDTO";
+import bcrypt from "bcryptjs";
+import { UserSignInDTO } from "../interface/UserSignInDTO";
+import { sc } from "../constants";
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
@@ -12,17 +16,17 @@ const getUserById = async (userId: number) => {
   return user;
 };
 
-//* 유저 생성
-const createUser = async(userName:string, email:string, age:number) => {
-  const data= await prisma.user.create({
-      data: {
-        userName: userName,
-        email: email,
-        age: age
-      }
-  });
-  return data;
-};
+// //* 유저 생성
+// const createUser = async(userName:string, email:string, age:number) => {
+//   const data= await prisma.user.create({
+//       data: {
+//         userName: userName,
+//         email: email,
+//         age: age
+//       }
+//   });
+//   return data;
+// };
 
 // * 유저 전체 조회
 const getAllUser = async() => {
@@ -53,6 +57,44 @@ const deleteUser = async(userId:number) => {
   });
 };
 
+const createUser = async (userCreateDto: UserCreateDTO) => {
+  //? 넘겨받은 password를 bcrypt의 도움을 받아 암호화
+  const salt = await bcrypt.genSalt(10); //^ 매우 작은 임의의 랜덤 텍스트 salt
+  const password = await bcrypt.hash(userCreateDto.password, salt); //^ 위에서 랜덤을 생성한 salt를 이용해 암호화
+
+  const data = await prisma.user.create({
+    data: {
+      userName: userCreateDto?.name,
+      age: userCreateDto?.age,
+      email: userCreateDto.email,
+      password,
+    },
+  });
+
+  return data;
+};
+
+const signIn = async (userSignInDto: UserSignInDTO) => {
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        email: userSignInDto.email,
+      },
+    });
+    if (!user) return null;
+
+    //? bcrypt가 DB에 저장된 기존 password와 넘겨 받은 password를 대조하고,
+    //? match false시 401을 리턴
+    const isMatch = await bcrypt.compare(userSignInDto.password, user.password);
+    if (!isMatch) return sc.UNAUTHORIZED;
+
+    return user.id;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
 const userService = {
-  getUserById,createUser, getAllUser, updateUser, deleteUser
+  getUserById,createUser, getAllUser, updateUser, deleteUser, signIn
 };
